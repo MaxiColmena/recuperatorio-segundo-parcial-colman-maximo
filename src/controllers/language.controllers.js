c27d6b3c1b15a2bd8bf8463f6604b9cc96d289a9
Colman Máximo <colmanmaximo2003@gmail.com>
2025-07-10 16:38:55 -0300

    La función de eliminar un lenguaje funciona con éxito

--- src/controllers/language.controllers.js
+++ src/controllers/language.controllers.js
@@ -11,4 +11,45 @@ export const createLanguage = async(req, res) => {
     } catch (error) {
         res.status(500).json({message: "Error al crear el lenguaje", error});
     }
-}
+};
+
+export const updateLanguage = async(req, res) => {
+    try {
+        const {name, paradigm, release_year} = req.body;
+        
+        // Validacion de titulo unico
+        const nameUnique = await Movies.findOne({where: {title}});
+        if (nameUnique) return res.status(400).json({message: "El nombre del lenguaje ya existe"});
+
+        const [updated] = await Language.update({name, paradigm, release_year});
+        if (updated > 0) return res.status(200).json({message:"El lenguaje fue actualizado con éxito"})
+        res.status(404).json({message: "El lenguaje no existe"})
+    } catch (error) {
+        return res.status(404).json({message: "Ese Lenguaje no existe en la base de datos"});
+    }
+};
+
+// export const getAllLanguage = async(req, res) => {
+//     try {
+        
+//     } catch (error) {
+        
+//     }
+// };
+
+// export const getByIdLanguage = async(req, res) => {
+//     try {
+        
+//     } catch (error) {
+        
+//     }
+// };
+
+export const deleteLanguage = async(req, res) => {
+    try {
+        const deleteLanguage = await Language.destroy({where: {id: req.params.id}});
+        if (deleteLanguage) return res.status(200).json({message: "El lenguaje fue eliminado"})
+    } catch (error) {
+        res.status(500).json({errormessage: err.message});
+    }
+};
